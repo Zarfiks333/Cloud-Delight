5002c89dbac4b16212394a4a4ed665be2f6108b2
Z҉a҉r҉f҉i҉k҉s҉3҉3҉3҉ <120936507+Zarfiks333@users.noreply.github.com>
2025-06-29 22:21:35 +0300

    Delete services directory

--- services/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class ServiceAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}  
-
-admin.site.register(Service, ServiceAdmin)
--- services/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from services.views import *
-
-app_name = 'services'
-
-urlpatterns = [
-    path('', indexView, name='serviceshome'),
-    path('service/<slug:slug>/', serviceDetail, name='service_detail'),
-]
