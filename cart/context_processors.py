995a9931644b3948005993b758a2a4c582c6a704
Z҉a҉r҉f҉i҉k҉s҉3҉3҉3҉ <120936507+Zarfiks333@users.noreply.github.com>
2025-06-29 22:15:07 +0300

    Delete cart directory

--- cart/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpRequest
-
-def cart_count(request: HttpRequest):
-    cart = request.session.get('cart', [])
-    cart_count = len(cart)
-    return {'cart_count': cart_count}
